53bdf88b75dce7be7e8c35ec1181328974410728
Patrick Abrena <patrickabrena@hotmail.com>
2024-01-14 22:28:54 -0600

    added slot-loop.js

---
+++ slot-loop.js
@@ -0,0 +1,10 @@
+/*
+-create slot machine effect / choosing an item 
+
+
+-My approach,
+    - create for loop to iterate through array of images for x amount time
+    - set timeOut or any other method to attach a unit of time to the indexed img
+    - make it so that it activates on click
+    - Possibly usee javascript es6 promise to offset the slot machine effect
+ */
