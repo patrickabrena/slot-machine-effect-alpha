b66275de3abffea21db5a48d20f7e51592a29bd9
Patrick Abrena <patrickabrena@hotmail.com>
2024-01-15 12:03:25 -0600

    create alpha of img_looping_effect();
    declare imageUrls arr with my images
    make display images function
    inside it, declare firstbox, secondbox, thirdbox in imageContainer arr
    add for loop to iterate through imageUrl arr with setTimeout
    assign displayTime val to imageUrl[i] with innerHTML property to set content inside
    nested for loop to target imageContainer[j] to iterate through imageContainer arr
    
    =======
    
    work on changing window on load to addEventLister possibly?
    figure out how to iterate through imageURL arr "x" amount of times

--- slot-loop.js
+++ slot-loop.js
@@ -8,3 +8,34 @@
     - make it so that it activates on click
     - Possibly usee javascript es6 promise to offset the slot machine effect
  */
+
+const img_looping_effect = () => {
+  const imageUrls = [
+    "../test-rock-png/imgs/rock-final.png",
+    "../test-rock-png/imgs/hand-final.png",
+    "../test-rock-png/imgs/scissors-final.png",
+  ];
+
+  const displayImages = () => {
+    const firstbox = document.getElementById("box-first-child");
+    const secondbox = document.getElementById("box-second-child");
+    const thirdbox = document.getElementById("box-third-child");
+
+    imageContainer = [firstbox, secondbox, thirdbox];
+
+    const displayTime = 500;
+    //iterate through imageUrls Array
+    for (let i = 0; i < imageUrls.length; i++) {
+      setTimeout(() => {
+        //displays the image
+        for (let j = 0; j < imageContainer.length; j++) {
+          imageContainer[j].innerHTML = `<img src="${
+            imageUrls[i]
+          }" alt="Image ${i + 1}">`;
+        }
+      }, i * displayTime);
+    }
+  };
+  window.onload = displayImages;
+};
+img_looping_effect();
